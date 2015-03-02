26a228b66f484cbe130e5b30ded9ca53dff5d515
Ubuntu <rob@pascal.rorpage-pascal.e1.internal.cloudapp.net>
2015-03-02 16:10:33 +0000

    updated index to point to my repo

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 //Redirect to the github project for information about the webhook
-header("Location: https://github.com/captn3m0/ifttt-webhook");
+header("Location: https://github.com/rorpage/ifttt-webhook");
